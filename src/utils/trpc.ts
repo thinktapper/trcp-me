83592bc8739a60844b97c668c3df291f898c0a85
Andrew Tapper <andrew@tapper.codes>
2022-10-16 20:29:17 -0500

    :label: use the typed router in trpc and client

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,3 +1,4 @@
 import { createReactQueryHooks } from '@trpc/react'
+import { AppRouter } from '../server/route/app.router'
 
-export const trcp = createReactQueryHooks<>()
+export const trcp = createReactQueryHooks<AppRouter>()
